ac683bb97c41d72666929fadb03aa4cb8c552d56
calvinwu <calvinwu4@gmail.com>
2018-01-28 06:45:48 -0500

    added salience

--- salience.py
+++ salience.py
@@ -1,18 +1,21 @@
 import requests
 
-def get_salience():
+def get_salience(content, coin):
     result = requests.post('https://language.googleapis.com/v1/documents:analyzeEntitySentiment?key='
                            'AIzaSyAYinBVEi8o8kXMdayxCWqDFVE0R5WekkU',
                            json={
 'document': {
- "content": "de",
+ "content": content,
  "type": "PLAIN_TEXT"
 },
 "encodingType": "UTF8"
 })
-    return result.json()
+    result = result.json()['entities']
+    for element in result:
+        if coin.lower() == element['name'].lower():
+            return element['salience']
 
 if __name__ == '__main__':
-    get_salience()
+    print(get_salience('Bitcoin Cash is a shit coin.', 'Bitcoin Cash'))
 
 
