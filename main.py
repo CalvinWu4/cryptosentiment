ec604732a9f79bedd9aa82083ac45948078f1b17
Diosdavi Lara <diosdavi20@gmail.com>
2018-01-27 23:07:05 -0500

    pull top 10 list

--- main.py
+++ main.py
@@ -15,19 +15,29 @@
 import webapp2
 import os
 import template
+import requests
+import requests_toolbelt.adapters.appengine
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
+        # Use the App Engine Requests adapter. This makes sure that Requests uses
+        # URLFetch.
+        requests_toolbelt.adapters.appengine.monkeypatch()
+
+        # gets the top 10 cryptocurrencies from the coinmarketcap API
+        payload = {'limit': '10'}
+        toptencryptos = requests.get('http://api.coinmarketcap.com/v1/ticker/', params=payload)
+
         template_values = {
             'greetings': 'Hi',
             'url': 'test_url',
             'url_linktext': 'test_linktext',
+            'cryptos': toptencryptos.json()
         }
         path = os.path.join(os.path.dirname(__file__), 'index.html')
 
         #self.response.headers['Content-Type'] = 'text/plain'
         self.response.write(template.render(path, template_values))
-
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
